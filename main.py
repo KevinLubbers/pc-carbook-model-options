1db54f9ae6511ba88f41962757e33d0247efb3fe
Kevin Lubbers <kelubbers@gmail.com>
2026-05-07 20:41:25 -0400

    (feat) added option_category field to data extraction and to the DB - prettified some short descriptions and removed extra ) in text - truncated first 2 digits of model_code to conform with PCS limit - receive option_category from extracting subheader and transforming to   PCS equivalent - need to figure out how to handle random compatibility popup on model   load. doesn't effect extraction of data, but prevents moving on to the   next model

--- main.py
+++ main.py
@@ -13,6 +13,20 @@ LOGIN_PASS = os.getenv("LOGIN_PASS")
 HEADLESS = os.getenv("HEADLESS", "true").lower() == "true"
 DB_URL = os.getenv("DB_URL")
 
+#start helper functions
+def header_to_category(header):
+    match header:
+        case "OPTION PACKAGE":
+            return "EQUIP"
+        case "PRIMARY PAINT":
+            return "EXT"
+        case "SEAT TRIM":
+            return "INT"
+        case "PORT INSTALLED OPTIONS":
+            return "IND"
+        case _:
+            return 
+
 
 # Connect to the database
 conn = sqlite3.connect(DB_URL)
@@ -28,14 +42,16 @@ c.execute("""
             model_code TEXT NOT NULL,
             option_code TEXT NOT NULL,
             option_name TEXT DEFAULT NULL,
+            option_category TEXT DEFAULT NULL,
             invoice_price REAL NOT NULL,
             msrp_price REAL NOT NULL,
             scrape_date TEXT DEFAULT (datetime('now', 'localtime'))
         )""")
 
 #SQL query used later to insert using executemany()
-sql = """INSERT INTO model_options (model_year, division, model, model_code, option_code, option_name, invoice_price, msrp_price)
-VALUES (?, ?, ?, ?, ?, ?, ?, ?)"""
+sql = """INSERT INTO model_options (model_year, division, model, model_code, option_code, option_name, option_category, invoice_price, msrp_price)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+#end helper functions
 
 #main loop
 def run():
@@ -74,7 +90,8 @@ def run():
                 #get text of all models (except the first one, which is a placeholder)
                 models = page.eval_on_selector_all(".component-selector-model-input option", "options => options.slice(1).map(option=> option.textContent)")
                 #loop through all models found
-                for model in models[:1]:
+                #add models[:x] to only loop through to the xth model
+                for model in models:
                     page.select_option('.component-selector-model-input', label=model)
                     time.sleep(2)
                     #get dynamically generated table data after selecting model
@@ -106,13 +123,14 @@ def run():
                             cells = row.query_selector_all("td")
                             row_data = [cell.inner_text().strip() for cell in cells]
                             row_data = [x.replace("$", "").replace(",", "") for x in row_data]
-                            #order of query is (year, division, model, model_code, option_code, option_name, invoice_price, msrp_price )
+                            #order of query is (year, division, model, model_code, option_code, option_name, option_category, invoice_price, msrp_price )
                             full_model_name = model + " " + row_data[1]
-                            model_code = row_data[0]
+                            #added [2:] to remove the first 2 digits of the model code for Hyundai(PCS has a limit of 10)
+                            model_code = row_data[0][2:]
                             option_name = None
-                            insert_tuple = (year, division, full_model_name, model_code, "*MDL", option_name, float(row_data[2]), float(row_data[3]))
+                            insert_tuple = (year, division, full_model_name, model_code, "*MDL", option_name, None, float(row_data[2]), float(row_data[3]))
                             data.append(insert_tuple)
-                            insert_tuple = (year, division, full_model_name, model_code, "DFRT", option_name, float(row_data[4]), float(row_data[4]))
+                            insert_tuple = (year, division, full_model_name, model_code, "DFRT", option_name, None, float(row_data[4]), float(row_data[4]))
                             data.append(insert_tuple)
                             #end *MDL and DFRT
                             #start enter model options
@@ -121,15 +139,31 @@ def run():
                             page.wait_for_selector('#select-new-vehicle-button')
                             page.click('#select-new-vehicle-button')
                             time.sleep(2)
-                            option_table = page.query_selector_all("tr.dataRow")
+                            #add an enter key press maybe to account for random compatibility pop ups? shouldn't cause any issue if no pop up
+                            #end dynamic menu selection, starting data extraction
+                            option_table = page.query_selector_all("tr.subheader, tr.dataRow")
+                            current_header = ""
                             for option in option_table:
-                                cells = option.query_selector_all("td")
-                                row_data = [cell.inner_text().strip() for cell in cells]
-                                row_data = [x.replace("$", "").replace(",", "") for x in row_data]
-                                #order of query is (year, division, model, model_code, option_code, option_name, invoice_price, msrp_price )
-                                print(row_data)
-                                insert_tuple = (year, division, full_model_name, model_code, row_data[1], row_data[2], float(row_data[4]), float(row_data[5]))
-                                data.append(insert_tuple)
+                                is_subheader = option.evaluate("el => el.classList.contains('subheader')")
+                                is_data = option.evaluate("el => el.classList.contains('dataRow')")
+                                #extract text from header and translate to a PCS category IF row is a header
+                                if is_subheader:
+                                    current_header = header_to_category(option.inner_text().strip()) 
+                                elif is_data:
+                                    cells = option.query_selector_all("td")
+                                    row_data = [cell.inner_text().strip() for cell in cells]
+                                    row_data = [x.replace("$", "").replace(",", "") for x in row_data]
+                                    #order of query is (year, division, model, model_code, option_code, option_name, option_category, invoice_price, msrp_price )
+                                    row_data[2] = row_data[2].split('\n')[0].rstrip()
+                                    row_data[2] = (
+                                        row_data[2][:-1].rstrip()
+                                        if row_data[2].rstrip().count('(') < row_data[2].rstrip().count(')')
+                                        else row_data[2].rstrip()
+                                    )
+                                    insert_tuple = (year, division, full_model_name, model_code, row_data[1], row_data[2], current_header, float(row_data[4]), float(row_data[5]))
+                                    print(insert_tuple)
+                                    data.append(insert_tuple)
+                            #wait 25 seconds per model before moving to the next then reopen the dynamic menu and start the loop over again
                             time.sleep(25)
                             page.click(".framework-header-selector-button")
                             time.sleep(3)
